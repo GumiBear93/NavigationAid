74d53a76822e8bafc3ccf4fc207dd6a8a20ef37c
GumiBear93 <florian.thuemer@gmx.de>
2021-08-03 13:41:45 +0200

    set up Jetpack Navigation

--- app/src/main/java/com/example/navigationaid/MainActivity.kt
+++ app/src/main/java/com/example/navigationaid/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.navigationaid
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
